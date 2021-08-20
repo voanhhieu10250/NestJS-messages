8e576e1141c4ce4b156bdbbc7394275aa55087e5
Vo Anh Hieu <voanhhieu10250@gmail.com>
2021-08-21 01:44:11 +0700

    adding DI

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,14 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
-
-  constructor() {
-    // Service is creating its own dependencies.
-    // which mean MessagesService can not work correctly unless it has MessagesRepository
-    // DONT DO THIS ON REAL APPS
-    this.messagesRepo = new MessagesRepository();
-  }
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
